8f1e79863a4b48dd2a15581f3bc9f6b377f8b084
KolorowyAleksander <KolorowyAleksander@users.noreply.github.com>
2017-03-20 01:38:47 +0100

    some more refactor

--- src/main/java/tomograph/Position.java
+++ src/main/java/tomograph/Position.java
@@ -8,7 +8,7 @@ import static java.lang.Math.*;
 
 public class Position {
 
-    public static Point findEmmitersPositions(double alpha, double r){
+    public static Point findEmmitersPositions(double alpha, double r) {
         alpha += 90;
         double x = r * cos(toRadians(alpha));
         double y = r * sin(toRadians(alpha));
@@ -16,23 +16,23 @@ public class Position {
         return new Point(x + r, y + r);
     }
 
-    public static List<Point> findDetectorsPositions(double alpha, double phi, int r, int n){
+    public static List<Point> findDetectorsPositions(double alpha, double phi, int r, int n) {
         double x = 0, y = 0;
-        List<Point> detectorsPositions = new ArrayList<Point>();
-        for(int i = 0; i < n; i++){
+        List<Point> detectorsPositions = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
             Point position = new Point(x, y);
             if (i == 0) {
-                x = r * cos(toRadians(alpha+90) + PI - (toRadians(phi) / 2));
-                y = r * sin(toRadians(alpha+90) + PI - (toRadians(phi) / 2));
+                x = r * cos(toRadians(alpha + 90) + PI - (toRadians(phi) / 2));
+                y = r * sin(toRadians(alpha + 90) + PI - (toRadians(phi) / 2));
             } else if (i > 0 && i < n - 1) {
-                x = r * cos(toRadians(alpha+90) + PI - (toRadians(phi) / 2) + (i * toRadians(phi) / (n - 1)));
-                y = r * sin(toRadians(alpha+90) + PI - (toRadians(phi) / 2) + (i * toRadians(phi) / (n - 1)));
+                x = r * cos(toRadians(alpha + 90) + PI - (toRadians(phi) / 2) + (i * toRadians(phi) / (n - 1)));
+                y = r * sin(toRadians(alpha + 90) + PI - (toRadians(phi) / 2) + (i * toRadians(phi) / (n - 1)));
             } else {
-                x = r * cos(toRadians(alpha+90) + PI + (toRadians(phi) / 2));
-                y = r * sin(toRadians(alpha+90) + PI + (toRadians(phi) / 2));
+                x = r * cos(toRadians(alpha + 90) + PI + (toRadians(phi) / 2));
+                y = r * sin(toRadians(alpha + 90) + PI + (toRadians(phi) / 2));
             }
-            position.x = x+200; //+200 bo javafx
-            position.y = y+200; //+200 bo javafx
+            position.x = x + r; //+r because we're in 1st quarter
+            position.y = y + r; //+r because we're in 1st quarter
             detectorsPositions.add(position);
         }
 
